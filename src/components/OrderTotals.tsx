682b80b93b2247937688ad9d7b0bb0b2d7bde9e9
dvcode <daviolivero2103@gmail.com>
2024-04-05 09:09:38 -0400

    Feat: Calculating consumption

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,12 +1,21 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types/types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
+
+function OrderTotals({order} : OrderTotalsProps) {
+    const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
 
-function OrderTotals() {
   return (
     <>
         <div className="space-y-3">
             <h2 className="font-black text-2xl">Totales y Propina:</h2>
             <p>Subtotal a pagar: {''}
-                <span className="font-bold">$0</span>
+                <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
             </p>
 
             <p>Propina: {''}
